a96e4921a4027c12496057d95bea01dc5adaf4aa
Bernat Brunet <bernat@nan-tic.com>
2024-10-18 17:23:50 +0200

    Change the way its control if the sale could set to draft. Allow always the moves are in draft or cancelled.
    Fix some code typo.

--- purchase.py
+++ purchase.py
@@ -19,32 +19,15 @@ class Purchase(metaclass=PoolMeta):
         cls._buttons['draft']['invisible'] = ~Eval('allow_draft', False)
         cls._buttons['draft']['depends'] += ['allow_draft']
 
-    @classmethod
-    def get_allow_draft(cls, purchases, name):
-        res = dict((x.id, False) for x in purchases)
-
-        for purchase in purchases:
-            if purchase.state in ('draft', 'done'):
-                continue
-            moves = [m.id for line in purchase.lines for m in line.moves
-                + line.moves_ignored
-                if m.state != 'draft']
-            moves_recreated = [m.id for line in purchase.lines
-                for m in line.moves_recreated]
-            if ((moves and not moves_recreated)
-                        or (moves and moves_recreated
-                            and sorted(moves) != sorted(moves_recreated))):
-                continue
-            invoices = [i for i in purchase.invoices + purchase.invoices_ignored
-                if i.state != 'draft']
-            invoices_recreated = [i for i in purchase.invoices_recreated]
-            if ((invoices and not invoices_recreated)
-                        or (invoices and invoices_recreated
-                            and sorted(invoices) != sorted(invoices_recreated))):
-                continue
-            # in case not continue, set to True
-            res[purchase.id] = True
-        return res
+    def get_allow_draft(self, name):
+        if (self.state in ('draft', 'done')
+                or any([m for line in self.lines for m in line.moves
+                        if m.state not in ('draft', 'cancelled')])
+                or any([x for line in self.lines for x in line.invoice_lines
+                        if x.invoice and x.invoice.state not in (
+                            'draft', 'cancelled')])):
+            return False
+        return True
 
     @classmethod
     def draft(cls, purchases):
@@ -55,6 +38,7 @@ class Purchase(metaclass=PoolMeta):
         InvoiceLine = pool.get('account.invoice.line')
         Invoice = pool.get('account.invoice')
         LineRecreated = pool.get('purchase.line-recreated-stock.move')
+        LineIgnored = pool.get('purchase.line-ignored-stock.move')
 
         moves = []
         shipments = []
@@ -64,26 +48,25 @@ class Purchase(metaclass=PoolMeta):
         for purchase in purchases:
             if not purchase.allow_draft:
                 continue
-            moves.extend([m for line in purchase.lines for m in line.moves])
-            shipments.extend([s for s in purchase.shipments])
-            shipment_return.extend([s for s in purchase.shipment_returns])
-            invoices.extend([i for i in purchase.invoices])
-            invoice_lines.extend([il for line in purchase.lines
-                for il in line.invoice_lines if not il.invoice])
+            moves += [m for line in purchase.lines for m in line.moves]
+            shipments += purchase.shipments
+            shipment_return += purchase.shipment_returns
+            invoices += purchase.invoices
+            invoice_lines += [il for line in purchase.lines
+                for il in line.invoice_lines if not il.invoice]
         if moves:
             line_recreateds = LineRecreated.search([
                     ('move', 'in', moves),
                     ])
-            if line_recreateds:
-                LineRecreated.delete(line_recreateds)
+            LineRecreated.delete(line_recreateds)
+            line_ignoreds = LineIgnored.search([
+                    ('move', 'in', moves),
+                    ])
+            LineIgnored.delete(line_ignoreds)
             Move.delete(moves)
-        if shipments:
-            Shipment.delete(shipments)
-        if shipment_return:
-            ShipmentReturn.delete(shipment_return)
-        if invoice_lines:
-            InvoiceLine.delete(invoice_lines)
-        if invoices:
-            Invoice.delete(invoices)
+        Shipment.delete(shipments)
+        ShipmentReturn.delete(shipment_return)
+        InvoiceLine.delete(invoice_lines)
+        Invoice.delete(invoices)
 
         super().draft(purchases)
